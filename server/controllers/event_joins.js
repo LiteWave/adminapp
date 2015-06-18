0b770433fc4943c35600d185fa1bc3d4bca5803e
tylerlandas <tylerlandas@hotmail.com>
2015-06-18 01:24:55 -0700

    Set the first user to join the event as the winner.
    Signed-off-by: tylerlandas <tylerlandas@hotmail.com>

--- server/controllers/event_joins.js
+++ server/controllers/event_joins.js
@@ -12,8 +12,11 @@ var mongoose = require('mongoose'),
  * Find event_join by id
  */
 exports.event_join = function(req, res, next, id) {
-//    EventJoin.load(id, function(err, event_join) {
-  EventJoin.findOne({_id: id}).populate('_user_locationId').exec(function (err, event_joint) {
+    EventJoin.load(id, function(err, event_join) {
+        console.log('in event_join constr');
+    // don't know WTF this is doing.
+    //EventJoin.findOne({ _id: req.params.event_joinId }).populate('_user_locationId').exec(function (err, event_joint) {
+        //console.log(err);
         if (err) return next(err);
         if (!event_join) return next(new Error('Failed to load event_join ' + id));
         req.event_join = event_join;
@@ -62,26 +65,58 @@ exports.create = function(req, res) {
       // see if there's an active event_liteshow object before going any further
     EventLiteShow.find_active(req.user_location._lw_eventId, 
       function(err, event_liteshow) {
-        if (err) {
-          res.render('No active event_liteshow error', {
+        if (err)
+        {
+            res.render('No active event_liteshow error', {
             status: 500
           });
-        } else {
-          
-          if( !event_liteshow) {
-            res.status(404);
-            res.send({error: 'event_liteshow not available'});
-            return;
-          } else {
-              var event_join = new EventJoin(req.body);
-              event_join.mobile_time_offset_ms = mobile_time_offset;
-              event_join._user_locationId = req.user_location._id;
-              event_join._event_liteshowId = event_liteshow._id;
-              
-              UserLocation.find({ _lw_eventId: req.user_location._lw_eventId }).exec(function (err, user_locations) {
-			event_join._winner_user_locationId = user_locations[0];
-		});
-          
+        }
+        else
+        {
+            console.log('Something WAS FOUND. req.user_location._lw_eventId is ' + req.user_location._lw_eventId);
+            if (!event_liteshow) {
+                console.log('error, event liteshow is null');
+                console.log(err);
+                res.status(404);
+                res.send({error: 'event_liteshow not available'});
+                return;
+            }
+            else
+            {
+              console.log('trying to create the EJ');
+             
+              // look for a winner and create the object.
+              UserLocation.find({ _lw_eventId: req.user_location._lw_eventId }).count(function (err, count) {
+                  var event_join = new EventJoin(req.body);
+                  event_join.mobile_time_offset_ms = mobile_time_offset;
+                  event_join._user_locationId = req.user_location._id;
+                  event_join._event_liteshowId = event_liteshow._id;
+
+                  if (err)
+                  {
+                    console.log('Err in find UL to set Winner. err=' + err);
+                    return;
+                  }
+                  else if (count <= 1)
+                  {
+                    event_join._winner_user_locationId = req.user_location._id;
+                  }
+
+                  // use the offset to set the time for this phone to start
+                  event_join.mobile_start_at = new Date(event_liteshow.start_at.getTime() - event_join.mobile_time_offset_ms);
+
+                  event_join.save(function (err) {
+                      if (err) {
+                          res.render('error', {
+                              status: 500
+                          });
+                      } else {
+                          res.jsonp(event_join);
+                      }
+                  });
+              });
+
+         
   //            if( Math.floor((Math.random()*10)+1) >= 5 )       // for now, 1/2 the joins will be winners
                 //event_join._winner_user_locationId = req.user_location._id;
   //            else
@@ -90,20 +125,7 @@ exports.create = function(req, res) {
               //event_join._winner_user_locationId = req.user_location._id;
               //event_liteshow._winner_user_locationId = req.user_location._id;
               //EventLiteShow
-              
-  
-                // use the offset to set the time for this phone to start
-             event_join.mobile_start_at = new Date(event_liteshow.start_at.getTime() - event_join.mobile_time_offset_ms);
-          
-              event_join.save(function(err) {
-                if (err) {
-                    res.render('error', {
-                        status: 500
-                     });
-                  } else {
-                      res.jsonp(event_join);
-                  }
-              });
+
             } // end else
           } // end else
       });  // end call back function for find_active
