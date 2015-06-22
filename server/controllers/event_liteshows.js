ceccf421b87e26a209726d1ec19e83ddf6a37fa7
tylerlandas <tylerlandas@hotmail.com>
2015-06-21 23:36:08 -0700

    Undo command change.
    Signed-off-by: tylerlandas <tylerlandas@hotmail.com>

--- server/controllers/event_liteshows.js
+++ server/controllers/event_liteshows.js
@@ -121,26 +121,26 @@ exports.user_liteshow = function (req, res) {
 
   seq.commands.push({ "c": black, "pl1": second_length });
   seq.commands.push({ "c": white, "pl1": second_length });
-  seq.commands.push({ "pif": "w", "c": red, "pl1": second_length });
+  seq.commands.push({ "c": red, "pl1": second_length });
   seq.commands.push({ "c": black, "pl1": second_length });
-  seq.commands.push({ "pif": "w", "c": white, "pl1": second_length });
+  seq.commands.push({ "c": white, "pl1": second_length });
   seq.commands.push({ "c": red, "pl1": second_length, "v": true });
 
   seq.commands.push({ "c": black, "pl1": third_length });
   seq.commands.push({ "c": white, "pl1": third_length });
-  seq.commands.push({ "pif": "w", "c": red, "pl1": third_length });
+  seq.commands.push({ "c": red, "pl1": third_length});
   seq.commands.push({ "c": black, "pl1": third_length });
-  seq.commands.push({ "pif": "w", "c": white, "pl1": third_length });
+  seq.commands.push({ "c": white, "pl1": third_length });
   seq.commands.push({ "c": red, "pl1": third_length });
 
   seq.commands.push({ "c": black, "pl1": fourth_length });
   seq.commands.push({ "c": white, "pl1": fourth_length });
-  seq.commands.push({ "pif": "w", "c": red, "pl1": fourth_length });
-  seq.commands.push({ "pif": "w", "c": black, "pl1": fourth_length });
-  seq.commands.push({ "pif": "w", "c": white, "pl1": fourth_length });
-  seq.commands.push({ "pif": "w", "c": red, "pl1": fourth_length, "v": true });
+  seq.commands.push({ "c": red, "pl1": fourth_length });
+  seq.commands.push({ "c": black, "pl1": fourth_length });
+  seq.commands.push({ "c": white, "pl1": fourth_length });
+  seq.commands.push({ "c": red, "pl1": fourth_length, "v": true });
 
-  seq.commands.push({ "pif": "w", "c": red, "pl1": fourth_length, "v": true, "strobe": 1 });
+  seq.commands.push({ "c": red, "pl1": fourth_length, "v": true, "strobe": 1 });
 
   seq.commands.push({ "pif": "w", "pt": "win", "c": "216,19,37", "pl1": 5000 });
   seq.commands.push({ "pif": "l", "c": black, "pl1": 500 });
