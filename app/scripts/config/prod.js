17c2efe30511dc944f9dd7144c0b02952266210c
David Anderson <davidmartinanderson@gmail.com>
2016-02-29 10:39:08 -0800

    updating the api url

--- app/scripts/config/prod.js
+++ app/scripts/config/prod.js
@@ -3,7 +3,7 @@
   /////////////
   window.config = {
     debug: false,
-    apiURL: 'http://main-1156949061.us-west-2.elb.amazonaws.com/api',
+    apiURL: 'http://www.litewaveinc.com/api',
     env: 'prod'
   };
 })();
