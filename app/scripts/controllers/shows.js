54716c0007086095e496294c496a55052591df44
tylerlandas <tylerlandas@hotmail.com>
2017-03-06 22:56:14 -0800

    Add new functionality without seating chart integration

--- app/scripts/controllers/shows.js
+++ app/scripts/controllers/shows.js
@@ -9,7 +9,6 @@ angular.module('liteWaveApp').controller('ShowsController',
     $scope.cmds = [];
     $scope.existingLayout = false;
     $scope.currentShowType = 0;
-    $scope.stadiumMap = $("#MapContainer");
     $scope.currentSections;
     $scope.currentGroupNumber = 0;
     $scope.myData = [];
@@ -566,13 +565,6 @@ angular.module('liteWaveApp').controller('ShowsController',
       var col;
       while (cmdsIndex < cmdsLength)
       {
-        //$scope.stadiumMap.tuMap("HighlightSection", $scope.currentSections[i]);
-
-        /*$scope.toggleSections($scope.myData[cmdsIndex].sectionList);
-        console.log("==============================================================");
-        console.log("0: cmdsIndex = " + cmdsIndex + ". $scope.myData[cmdsIndex].sectionList=" + $scope.myData[cmdsIndex].sectionList);
-        console.log("==============================================================");*/
-
         $scope.executeCmdA(cmdsIndex, testCmds[cmdsIndex].commandList);
         
         cmdsIndex++;
@@ -627,16 +619,16 @@ angular.module('liteWaveApp').controller('ShowsController',
 
     $scope.toggleSections = function (command, sectionList)
     {
-      var length = sectionList.length;
+      /*var length = sectionList.length;
       for (var i = 0; i < length; i++)
       {
         $scope.stadiumMap.tuMap(command, sectionList[i]);  //"ToggleSelection"
-      }
+      }*/
     }
 
     $scope.createGroup = function ()
     {
-      $scope.currentSections = $scope.stadiumMap.tuMap("GetSelectedSections", {});
+      /*$scope.currentSections = $scope.stadiumMap.tuMap("GetSelectedSections", {});
       $scope.myData.push({ "id": $scope.currentGroupNumber, "sectionList" : $scope.currentSections });
       $scope.currentGroupNumber++;
 
@@ -645,7 +637,7 @@ angular.module('liteWaveApp').controller('ShowsController',
       for (var i = 0; i < length; i++)
       {
         $scope.stadiumMap.tuMap("ToggleSelection", $scope.currentSections[i]);
-      }
+      }*/
     };
 
     $scope.changeLayout = function (stadiumLayout)
@@ -690,49 +682,7 @@ angular.module('liteWaveApp').controller('ShowsController',
 
     if ($rootScope.currentClient && $rootScope.currentClient.externalStadiumId)
     {
-      /************************************************************
-      Initialize Ticket Utils Interactive Map
-      ************************************************************/
-      $scope.stadiumMap.tuMap({
-        //MapId: "24d98d09-37e1-437f-87c5-eae845692e6c"
-        MapId: $rootScope.currentClient.externalStadiumId
-            , MapType: "Interactive"
-            , ControlsPosition: "Inside"
-        /*Failover Map: Replace this with a URL of the static chart from alternate datasource(when available)*/
-			      , FailoverMapUrl: "http://static.ticketutils.com/Charts/No-Seating-Chart.jpg"
-			      , Tickets: Data.Tickets
-			      , AutoSwitchToStatic: false
-			      , PreferredFirst: false
-			      , TicketsListContainer: "#InventoryContainer"
-			      , GroupsContainer: "#GroupsContainer"
-			      , OnError: function (e, Error)
-			      {
-			        if (Error.Code == 0)
-			        {
-			          var Message = "<div style=\"padding:10px;\">";
-			          Message += "<span style=\"color:red;font-weight:bold;\">This Sample is Configured to run under host 'localhost'</span>";
-			          Message += "<br />";
-			          Message += "Please configure IIS/Apache or Compatible Web Server to point 'demo' folder in order to view the Sample. If you intend to Run it under different Domain, please contact TicketUtils Support for Activation";
-			          Message += "</div>";
-			          $("#MapContainer").html(Message);
-			        }
-			      },
-        OnBeforeListRender: function ()
-        {
-          var Height = $("#TuMap").outerHeight();
-          $("#InventoryContainer").height(Height);
-        },
-        OnInit: function (e, MapType)
-        {
-        },
-        OnClick: function (e, Section)
-        {
-        },
-        OnGroupClick: function (e, Group)
-        {
-        }
-      });
-    };
+    }
 
     $scope.saveLayout = function ()
     {
